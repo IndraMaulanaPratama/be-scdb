049a45b36cc6897442400ffe01e73d74e35aed69
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-09-27 15:25:48 +0700

    UPDATE || Registering biro to validation

--- sources/validations/index.js
+++ sources/validations/index.js
@@ -5,3 +5,4 @@ export * from "./app-location-validation.js";
 export * from "./satuan-kerja-validation.js";
 export * from "./lembaga-validation.js"
 export * from "./fakultas-validation.js"
+export * from "./biro-validation.js"
